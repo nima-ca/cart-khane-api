2897f71b44948b5291bb8585ad532599ecb1db25
Nima <nekoueinima@gmail.com>
2025-01-05 02:03:08 +0330

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
@@ -10,6 +11,24 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.use(helmet());
+
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    if (!isProduction) {
+        const config = new DocumentBuilder()
+            .setTitle('Cart Khane')
+            .setDescription('The Cart Khane API description')
+            .setVersion('1.0')
+            .addBearerAuth()
+            .build();
+
+        const documentFactory = () =>
+            SwaggerModule.createDocument(app, config, {
+                autoTagControllers: true,
+            });
+
+        SwaggerModule.setup('swagger', app, documentFactory);
+    }
 
     app.useGlobalPipes(new ValidationPipe(validationPipeOptions));
 
